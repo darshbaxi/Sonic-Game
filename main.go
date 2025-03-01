d001b411ce920b48422a600c5761b711a7d703aa
darshbaxi <baxidarsh@gmail.com>
2025-03-01 23:04:40 +0530

    deployment changes

--- main.go
+++ main.go
@@ -1,3 +1,74 @@
+// package main
+
+// import (
+// 	"bytes"
+// 	"flag"
+// 	"io/ioutil"
+// 	"log"
+// 	"net/http"
+// )
+
+// var addr = flag.String("addr", "127.0.0.1:8007", "http service address")
+// var serveFiles = flag.Bool("serveFiles", true, "use this app to serve static files (js, css, images)")
+// var gameLogDir = flag.String("gameLogDir", "/var/log/durak", "dir to store game logs")
+// var appEnv = flag.String("env", "local", "application environment: local, production")
+
+// var indexPageContent []byte
+
+// func serveIndexPage(w http.ResponseWriter, r *http.Request) {
+// 	if r.URL.Path != "/" {
+// 		http.Error(w, "Not found", 404)
+// 		return
+// 	}
+// 	if r.Method != "GET" {
+// 		http.Error(w, "Method not allowed", 405)
+// 		return
+// 	}
+// 	w.Write(indexPageContent)
+// }
+
+// func faviconHandler(w http.ResponseWriter, r *http.Request) {
+// 	http.ServeFile(w, r, "html/favicon.ico")
+// }
+
+// func main() {
+// 	flag.Parse()
+
+// 	indexPageContentRaw, err := ioutil.ReadFile("html/index.html")
+// 	if err != nil {
+// 		log.Fatal("Read index.html error: ", err)
+// 	}
+// 	version, err := ioutil.ReadFile("version")
+// 	if err != nil {
+// 		log.Println("Cannot read file 'version': ", err)
+// 	}
+// 	indexPageContent = bytes.Replace(indexPageContentRaw, []byte("%APP_ENV%"), []byte(*appEnv), 1)
+// 	indexPageContent = bytes.Replace(indexPageContent, []byte("%APP_VERSION%"), bytes.TrimSpace([]byte(version)), 2)
+
+// 	gameLogger := NewGameFileLogger(*gameLogDir, func(err error) {
+// 		log.Println(err)
+// 	})
+
+// 	lobby := newLobby(gameLogger)
+// 	go lobby.run()
+// 	http.HandleFunc("/", serveIndexPage)
+// 	if *serveFiles {
+// 		http.HandleFunc("/favicon.ico", faviconHandler)
+// 		http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./html/js"))))
+// 		http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./html/css"))))
+// 		http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("./html/img"))))
+// 	}
+// 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+// 		serveWs(lobby, w, r)
+// 	})
+// 	log.Printf("Listening http://%s", *addr)
+// 	err = http.ListenAndServe(*addr, nil)
+// 	if err != nil {
+// 		log.Fatal("ListenAndServe: ", err)
+// 	}
+// }
+
+
 package main
 
 import (
@@ -6,22 +77,31 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
-var addr = flag.String("addr", "127.0.0.1:8007", "http service address")
 var serveFiles = flag.Bool("serveFiles", true, "use this app to serve static files (js, css, images)")
 var gameLogDir = flag.String("gameLogDir", "/var/log/durak", "dir to store game logs")
 var appEnv = flag.String("env", "local", "application environment: local, production")
 
+var addr string
 var indexPageContent []byte
+
+func init() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8007" // Default port if not specified
+	}
+	addr = ":" + port // Updated to work on Render
+}
 
 func serveIndexPage(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.Error(w, "Not found", 404)
+		http.Error(w, "Not found", http.StatusNotFound)
 		return
 	}
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", 405)
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 	w.Write(indexPageContent)
@@ -43,7 +123,7 @@ func main() {
 		log.Println("Cannot read file 'version': ", err)
 	}
 	indexPageContent = bytes.Replace(indexPageContentRaw, []byte("%APP_ENV%"), []byte(*appEnv), 1)
-	indexPageContent = bytes.Replace(indexPageContent, []byte("%APP_VERSION%"), bytes.TrimSpace([]byte(version)), 2)
+	indexPageContent = bytes.Replace(indexPageContent, []byte("%APP_VERSION%"), bytes.TrimSpace(version), 2)
 
 	gameLogger := NewGameFileLogger(*gameLogDir, func(err error) {
 		log.Println(err)
@@ -51,6 +131,7 @@ func main() {
 
 	lobby := newLobby(gameLogger)
 	go lobby.run()
+
 	http.HandleFunc("/", serveIndexPage)
 	if *serveFiles {
 		http.HandleFunc("/favicon.ico", faviconHandler)
@@ -61,8 +142,9 @@ func main() {
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(lobby, w, r)
 	})
-	log.Printf("Listening http://%s", *addr)
-	err = http.ListenAndServe(*addr, nil)
+
+	log.Printf("Listening on http://%s", addr)
+	err = http.ListenAndServe(addr, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
